2ae1e32281efa5a4b8f6d259b2c1a9139bac6aec
jorgesabella <jorge_andres1996@hotmail.com>
2020-07-08 16:04:06 -0500

    test

---
+++ data.js
@@ -0,0 +1,75 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-microsoftteams-image",
+      "name": "MicrosoftTeams-image",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 256,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11648184396819694,
+          "pitch": 0.21560400945816838,
+          "rotation": 0,
+          "target": "1-attachment-17624"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-attachment-17624",
+      "name": "Attachment-1[7624]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.22640165995733952,
+        "pitch": 0.21138655156713426,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2191878791220212,
+          "pitch": 0.2398627018760635,
+          "rotation": 0,
+          "target": "0-microsoftteams-image"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
